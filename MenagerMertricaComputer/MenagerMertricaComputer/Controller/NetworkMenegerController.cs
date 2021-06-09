70929a7e683f4474c5be5a1433b7bde6a43b8a22
nickchuchunov <nick.chuchunov@yandex.u>
2021-06-09 22:26:14 +0300

    внесение поправок

--- MenagerMertricaComputer/MenagerMertricaComputer/Controller/NetworkMenegerController.cs
+++ MenagerMertricaComputer/MenagerMertricaComputer/Controller/NetworkMenegerController.cs
@@ -11,17 +11,22 @@ namespace MenagerMertricaComputer.Controller
     [ApiController]
     public class NetworkMenegerController : ControllerBase
     {
+
+        [Route("api/metrics/cpu//from/{fromTime}/to/{toTime}/")]
         [HttpGet("api/metrics/cpu//from/{fromTime}/to/{toTime}/")]
         public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
         {
             return Ok();
         }
-
+        [Route("api/metrics/dotnet/errors-count/from/{fromTime}/to/{toTime}")]
         [HttpGet("api/metrics/dotnet/errors-count/from/{fromTime}/to/{toTime}")]
         public IActionResult GetMetricsByPercentileFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
         {
             return Ok();
         }
+
+
+        [Route("api/metrics/network/from/{fromTime}/to/{toTime}/")]
         [HttpGet("api/metrics/network/from/{fromTime}/to/{toTime}/")]
 
         public IActionResult GetMetricsFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
@@ -29,11 +34,14 @@ namespace MenagerMertricaComputer.Controller
             return Ok();
         }
 
+
+        [Route("api/metrics/cpu/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
         [HttpGet("api/metrics/cpu/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
         public IActionResult GetMetricsByPercentileFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime, [FromRoute] Percentile percentile)
         {
             return Ok();
         }
+
 
 
 
